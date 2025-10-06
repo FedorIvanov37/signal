e26883939d945719801424e242403c1f52b605f6
Fedor Ivanov <116465333+FedorIvanov37@users.noreply.github.com>
2025-10-07 02:47:23 +0300

    Update signal.py
    Signed-off-by: Fedor Ivanov <116465333+FedorIvanov37@users.noreply.github.com>

--- common/signal.py
+++ common/signal.py
@@ -1,10 +1,16 @@
-#
-# SIGNAL starting file
-#
-# This file runs SIGNAL GUI. The GUI runs once the file is imported, no additional actions are required
-#
-# e.g.: "from common import signal"
-#
+"""
+
+Signal starting script 
+
+This script runs Signal GUI or CLI, depending on start arguments
+
+The script starts GUI or CLI only. If you're using library see application example is the User Reference Guide
+
+The Signal started once the file is imported, no additional actions are required
+
+Example of the script run command: "from common import signal"
+
+"""
 
 
 __author__ = "Fedor Ivanov"
@@ -13,6 +19,9 @@ __version__ = "v0.19"
 
 # Correct way to run
 if __name__ != "__main__":  # Runs only by import command
+
+    # Preparation to run the Signal 
+    
     try:
         from sys import exit, argv
         from os import makedirs
@@ -23,20 +32,27 @@ if __name__ != "__main__":  # Runs only by import command
         from PyQt6.QtCore import QLoggingCategory
         from common.lib.core.CustomConfigFile import CustomConfigFile
 
-        custom_config: CustomConfigFile = CustomConfigFile(add_help=False)
+        custom_config: CustomConfigFile = CustomConfigFile(add_help=False)  # Config file can be rewritten in CLI move
 
-        config_file = custom_config.get_config_filename()
+        config_file = custom_config.get_config_filename()  # Get the config file name
         
-        with open(config_file) as json_file:
+        with open(config_file) as json_file:  # Read the config
             config: Config = Config.model_validate_json(json_file.read())
 
-        for directory in TermDirs:
+        for directory in TermDirs:  # Create important priject directories in case when some of them don't exist
             makedirs(directory, exist_ok=True)
-
+        
+        QLoggingCategory.setFilterRules(  # Reduce redundant log messages from the output
+            """qt.multimedia.*=false 
+               qt.multimedia.ffmpeg.*=false"""
+        )
+        
     except Exception as run_preparation_error:
         print(run_preparation_error)
         exit(100)
 
+    # Preparation is finished, let's start it
+    
     cli_mode_triggers: tuple = (
         CliDefinition.CONSOLE_MODE,
         CliDefinition.CONSOLE_MODE_LONG,
@@ -44,18 +60,13 @@ if __name__ != "__main__":  # Runs only by import command
         CliDefinition.HELP_LONG,
     )
 
-    if any(arg in argv for arg in cli_mode_triggers):  # Run in Command Line Interface (CLI) mode
-        cli: SignalCli = SignalCli(config)
+    if any(arg in argv for arg in cli_mode_triggers):  # In case when CLI mode requested
+        cli: SignalCli = SignalCli(config)  # Run in Command Line Interface (CLI) mode
         cli.run_application()
-        exit(int())
+        exit(int())  # GUI won't be ran after CLI
 
-    try:  # Run in Graphic User Interface (GUI) mode
-
-        QLoggingCategory.setFilterRules(  # Reduce redundant log messages from the output
-            """qt.multimedia.*=false 
-               qt.multimedia.ffmpeg.*=false"""
-        )
-
+    try:  # Run Graphic User Interface (GUI) mode
+        
         from common.gui.core.SignalGui import SignalGui
 
         terminal: SignalGui = SignalGui(config)
@@ -72,9 +83,9 @@ if __name__ != "__main__":  # Runs only by import command
 # Incorrect way to run
 if __name__ == "__main__":  # Do not run directly
     error_message = """
-The file common/common.py should be imported from the main working directory, the direct run has no effect
+The file common/signal.py should be imported from the main working directory, the direct run has no effect
 The GUI runs once the file is imported, no additional actions are required
-Please, refer to the SvTerminal documentation to read more about how to run the application
+Please, refer to the Signal documentation to read more about how to run the application
 """
 
     raise RuntimeError(error_message)
